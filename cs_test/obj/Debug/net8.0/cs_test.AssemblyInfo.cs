9bd6e9f8a84a34a66123ac640677ddc825b908e9
yoohyeonseung <probability@kakao.com>
2024-03-06 16:57:40 +0900

    ㅎㅎ

--- cs_test/obj/Debug/net8.0/cs_test.AssemblyInfo.cs
+++ cs_test/obj/Debug/net8.0/cs_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21fbf6f63a19f8f3200a332f6f7a386b98f11f7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36be41219557cfa168822d0625e4072199c1709f")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
