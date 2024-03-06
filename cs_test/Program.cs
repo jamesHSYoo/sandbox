0d49fd3f7b0a3aa44f977e557df2ae5d3be563ec
yoohyeonseung <probability@kakao.com>
2024-03-06 17:05:40 +0900

    뭐 할때마다 뭔가 많이 생성됨 뭔지 모르겠음

--- cs_test/Program.cs
+++ cs_test/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+
+    public static void Main(String[] args){
+
+        Console.WriteLine("Hello, Dotnet Program");
+        
+    }
+}
--- cs_test/obj/Debug/net8.0/cs_test.AssemblyInfo.cs
+++ cs_test/obj/Debug/net8.0/cs_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36be41219557cfa168822d0625e4072199c1709f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd6e9f8a84a34a66123ac640677ddc825b908e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
