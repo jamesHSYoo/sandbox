526502a8b8d495818a4fdb9731670cc22687e6b9
probability@kakao.com <probability@kakao.com>
2024-03-26 22:20:25 +0900

    c++ input output 1

---
+++ cpp/input_output/input_output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int i, j;
+
+    cout << "Enter num_1 : ";
+    cin >> i;
+
+    cout << "Enter num_2 : ";
+    cin >> j;
+
+    cout << "num_1 + num_2 = " << i + j << std::endl;
+
+    return 0;
+
+}
