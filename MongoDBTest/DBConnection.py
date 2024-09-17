815f50d526b75da526935fb23ba68cce69cc1ad9
yoohyeonseung <probability@kakao.com>
2024-09-17 21:19:46 +0900

    MongoDB find and Insert

--- MongoDBTest/DBConnection.py
+++ MongoDBTest/DBConnection.py
@@ -34,6 +34,16 @@ if __name__ == '__main__':
     db = DB()
     db.connection_mongo_db()
     collection = db.set_collection("admin", "price_data")
+    # Data insert
+    collection.insert_one(
+        {
+            "_id" : "KOSPI200@20240911",
+            "closed_price" : "335.65",
+            "base_dt" : "20240911",
+            "asset_code" : "KOSPI200"
+        }
+    )
+    # Data find
     results = collection.find_one({ "asset_code" : "KOSPI200" })
     print(results)
     db.disconnect_connection()
