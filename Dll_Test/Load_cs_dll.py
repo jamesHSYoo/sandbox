5ccb5bb4daf45e393c1d115b928b687e3d1dd605
probability@kakao.com <probability@kakao.com>
2025-03-29 14:50:05 +0900

    설명 및 함수 테스트 추가

--- Dll_Test/Load_cs_dll.py
+++ Dll_Test/Load_cs_dll.py
@@ -1,15 +1,22 @@
 from pythonnet import load
 import os 
 
-load("coreclr")
+load("coreclr") # 지정해주지 않으면 default 는 mono로 되어 있음
 path = os.getcwd()
-
 
 import clr
 
 clr.AddReference(path + "/sandbox/DLL_Test/DllTest.dll")
 
 from DllTest import DllTest
-
 p = DllTest()
+"""
+C# dll 로드 방식 : 
+from {namespace} import {class 명}
+
+p.{class 명}() => c#에서 생성자 생성방식과 동일한듯 
+ex) DllTest p = new DllTest()
+"""
 p.PrintHello()
+result = p.Add(1, 2)
+print(result)
